71ff07e5a78f4211e2b827e5864b86cf51e14cff
Heshitha <hesithedg@gmail.com>
2024-09-05 05:16:58 +0530

    add user msg function and link with html

--- 3.gemini-clone/script.js
+++ 3.gemini-clone/script.js
@@ -1,9 +1,45 @@
-const typingForm =document.querySelectorAll(".typing-form");
+const typingForm = document.querySelector(".typing-form");
+const chatList = document.querySelector(".chat-list");
 
-// prevent default form submisson and handle outgoing chat
-typingForm.addEventListener("submit",(e) => {
+let userMessage = null;
+
+
+//create a new message element and return it
+const createMessageElement = (content, className) => {
+  const div = document.createElement("div");
+  div.classList.add("message",className);
+  div.innerHTML = content;
+  return div;
+}
+
+
+// handle sending outgoing messages
+const handleOutgoingchat = () => {
+  userMessage = typingForm.querySelector(".typing-input").value.trim();
+  if (!userMessage) return; //exit if there is no message
+
+  // console.log(userMessage);
+  const html = ` <div class="message-content">
+                <img src="./images/user.jpg" alt="user image" class="avater">
+                <p class="text"></p>
+              </div>`;
+
+  const outgoingMessageDiv = createMessageElement(html, "outgoing");
+  outgoingMessageDiv.querySelector(".text").innerHTML = userMessage;
+  chatList.appendChild(outgoingMessageDiv);
+};
+
+// prevent default form submission and handle outgoing chat
+typingForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  handleOutgoingChat();
-  
-})
+  handleOutgoingchat();
+});
+
+
+
+
+
+
+
+
